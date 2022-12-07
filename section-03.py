6c2cb6a16e9860791ec39e03807652884960c978
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-07 22:50:25 +0330

    minor change

--- section-03.py
+++ section-03.py
@@ -1,15 +1,15 @@
 # Polymorphism is the ability to take many forms
 # Suppose we start with the following code:
 class Vehicle:
-    def __str__(self) -> str:
+    def __str__(self):
         return "Object: Vehicle"
 
 
 class Car:
-    def __str__(self) -> str:
+    def __str__(self):
         return "Object: Car"
 
 
 class Airplane:
-    def __str__(self) -> str:
+    def __str__(self):
         return "Object: Airplane"
