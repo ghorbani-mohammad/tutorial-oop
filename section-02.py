2d432ccc2d04d2ebf8f58aa573d4045d4ba86f01
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-07 21:55:40 +0330

    add class Employee to section-02

--- section-02.py
+++ section-02.py
@@ -1,2 +1,15 @@
 # Here we have another example about using super and overriding (check the previous section) but
 # We also want to add polymorphism into it.
+
+
+class Employee:
+    BONUS_PER_YEAR = 1000
+
+    def __init__(self, name, base_salary, years):
+        self.name = name
+        self.base_salary = base_salary
+        self.years = years
+
+    def salary(self):
+        return self.base_salary + (self.years * self.BONUS_PER_YEAR)
+
