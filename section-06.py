115ec74dbc688ecea585045b55724033baeb2b36
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-08 19:39:24 +0330

    add class Computer to section-06

--- section-06.py
+++ section-06.py
@@ -3,3 +3,14 @@
 # Using OOP in Python, we can restrict access to methods and variables.
 # This prevents data from direct modification which is called encapsulation.
 # In Python, we denote private attributes using underscore as the prefix i.e single _ or double __.
+
+
+class Computer:
+    def __init__(self):
+        self.__max_price = 900
+
+    def sell(self):
+        print(f"Selling Price: {self.__max_price}")
+
+    def setMaxPrice(self, price):
+        self.__max_price = price
