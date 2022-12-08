1da11b8a8891242f0c69bb3199a02f126dec391f
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-08 13:22:55 +0330

    add class Car to section-5

---
+++ section-05.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, door, wheel):
+        self.door = door
+        self.wheel = wheel
+
+    def start(self):
+        print("Start the car")
+
+    def go(self):
+        print("Going")
