3c3fd55291c13b7b0beb3afdf230566b5c46b53b
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-08 20:07:52 +0330

    minor change

--- section-06.py
+++ section-06.py
@@ -3,6 +3,8 @@
 # Using OOP in Python, we can restrict access to methods and variables.
 # This prevents data from direct modification which is called encapsulation.
 # In Python, we denote private attributes using underscore as the prefix i.e single _ or double __.
+# Single _ means protected variables
+# Double __ means private variables
 
 
 class Computer:
