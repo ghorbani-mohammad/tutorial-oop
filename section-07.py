5b81dc4aca3dd37d8c7131309ef50e402c5a3ecc
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-09 13:13:15 +0330

    add Derived class to section-07

--- section-07.py
+++ section-07.py
@@ -1,3 +1,12 @@
 class Base:
     def __init__(self):
         self._a = 2
+
+
+class Derived(Base):
+    def __init__(self):
+        Base.__init__(self)
+        print("Calling protected member of base class: ", self._a)
+
+        self._a = 3
+        print("Calling modified protected member outside class: ", self._a)
